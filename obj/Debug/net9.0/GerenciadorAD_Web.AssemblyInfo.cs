73ff7c1ed1ce3f77e5c057544aefbc9080482f72
Bahryz <phbahry@gmail.com>
2025-11-10 11:23:47 -0300

    envio8

--- obj/Debug/net9.0/GerenciadorAD_Web.AssemblyInfo.cs
+++ obj/Debug/net9.0/GerenciadorAD_Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GerenciadorAD_Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49f19120395e0f2aefaab59eb2042bf231597483")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5203be1486871d14e56e3516a2b019f9e4ea482b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GerenciadorAD_Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GerenciadorAD_Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
